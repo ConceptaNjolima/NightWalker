e414217749a8f15ef0d103629f82fb770a6904ee
Sama Manalai <manalais@berea.edu>
2020-04-04 21:23:46 -0400

    loginUser is created

--- app/src/main/java/com/example/nightwalker/LoginActivity.java
+++ app/src/main/java/com/example/nightwalker/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.example.nightwalker;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,5 +25,27 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Username = findViewById(R.id.Username);
+        Password = findViewById(R.id.Password);
+        Loginbutton = findViewById(R.id.Loginbutton);
+
+        Loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Login button");
+                String username = Username.getText().toString();
+                String password = Password.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+        private void loginUser(String username, String password){
+            Log.i(TAG, "Attempting to login user" + username);
+            // TO DO: We should go to mainActivity once user is signed in. Sama
+        }
+
+
     }
 }
