62826ccf2e37a57464caff0bac5dc8816c16334b
ConceptaNjolimagit config --global user.email njolimac@berea.edugit config --global user.name ConceptaNjolima <njolimac@berea.edu>
2020-04-12 12:45:35 -0400

    Added button to make phone call to Police

--- app/src/main/java/com/example/nightwalker/MainActivity.java
+++ app/src/main/java/com/example/nightwalker/MainActivity.java
@@ -1,19 +1,61 @@
 package com.example.nightwalker;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     //Created the mainActivity which connects to activity_main. Sama
 
     public static final String TAG = "MainActivity";
+    private static final int REQUEST_CALL=1;
+    public static final String CALL_NUMBER="8599791217";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageCall=findViewById(R.id.image_call);
+        imageCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall();
+            }
+        });
+    }
+
+    private void makePhoneCall() {
+        if(ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
+        }else{
+            String dial="tel:"+CALL_NUMBER;
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode==REQUEST_CALL){
+            if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                makePhoneCall();
+            }
+            else{
+                Toast.makeText(this,"Permission Denied",Toast.LENGTH_SHORT);
+            }
+        }
     }
 }
