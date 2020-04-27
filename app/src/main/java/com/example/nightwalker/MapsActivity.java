ebe9dfc003c7f7a13df31dc05440873feb4603ca
ConceptaNjolimagit config --global user.email njolimac@berea.edugit config --global user.name ConceptaNjolima <njolimac@berea.edu>
2020-04-26 23:55:07 -0400

    Enabled sending of location via SMS to Police

--- app/src/main/java/com/example/nightwalker/MapsActivity.java
+++ app/src/main/java/com/example/nightwalker/MapsActivity.java
@@ -1,15 +1,24 @@
 package com.example.nightwalker;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -26,19 +35,61 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MAP" ;
     Location currentlocation;
     FusedLocationProviderClient fusedLocationProviderClient;
     private static final int REQUEST_CODE = 101;
+    private static final int SEND_SMS_REQUEST_CODE = 1;
     private BottomNavigationView bottomNavigationView;
+    private Button ShareLtn;
+
+
+
+    public boolean checkPermission(String permission){
+        int check= ContextCompat.checkSelfPermission(this,permission);
+        return (check==PackageManager.PERMISSION_GRANTED);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        ShareLtn=findViewById(R.id.btnShareLtn);
+
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
         fetchLastLocation();
+/*
+        ContentResolver resolver =getContentResolver();
+        Cursor cursor= resolver.query(ContactsContract.Contacts.CONTENT_URI,null,
+                null,null,null);
+        while(cursor.moveToNext()){
+            String id=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String name=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
 
+            Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "= ? ",new String[]{id},null);
+            Log.i(TAG, "CONTACTS"+ id +"name"+name);
+        }
+*/
+        ShareLtn.setEnabled(false);
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            ShareLtn.setEnabled(true);
+        }
+        else{
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.SEND_SMS},SEND_SMS_REQUEST_CODE);
+        }
+        ShareLtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSend(v);
+
+            }
+
+
+
+        });
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -59,7 +110,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
     }
+    public void onSend(View v){
+        String PhoneNumber="8599791217";
+        String message= currentlocation.getLatitude()+ ""+ currentlocation.getLongitude();
+        if(checkPermission(Manifest.permission.SEND_SMS)){
+            SmsManager smsManager= SmsManager.getDefault();
+            smsManager.sendTextMessage(PhoneNumber,null,message,null, null);
+            Toast.makeText(this, "Sending location "+ currentlocation.getLatitude()+""+currentlocation.getLongitude(), Toast.LENGTH_SHORT).show();
 
+
+        }
+        else{
+            Toast.makeText(this, "Failed to send message", Toast.LENGTH_SHORT).show();
+        }
+    }
     private void fetchLastLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
@@ -71,11 +135,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onSuccess(Location location) {
                 if (location != null){
                     currentlocation = location;
-                    Toast.makeText(getApplicationContext(), currentlocation.getLatitude()+""+currentlocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(getApplicationContext(), currentlocation.getLatitude()+""+currentlocation.getLongitude(), Toast.LENGTH_SHORT).show();
                     SupportMapFragment supportMapFragment = (SupportMapFragment)
                             getSupportFragmentManager().findFragmentById(R.id.map);
                     supportMapFragment.getMapAsync(MapsActivity.this);
                 }
+
+
+
             }
         });
     }
